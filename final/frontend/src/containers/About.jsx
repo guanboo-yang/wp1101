58b085349ed40750ba2d57dcdd9fc5cb51845437
Guan-Bo Yang <b09902064@ntu.edu.tw>
2022-01-16 18:41:54 +0800

    [1.0.1] add about

--- final/frontend/src/containers/About.jsx
+++ final/frontend/src/containers/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
 		fontSize: '25px',
 	}
 	return (
-		<div style={{ textAlign: 'left' }}>
+		<div style={{ textAlign: 'center' }}>
 			<h1>How to play?</h1>
 			<p>
 				Press <span style={style}>enter</span> to <span style={style}>turn</span>
@@ -14,12 +14,6 @@ const About = () => {
 				Press <span style={style}>space</span> to <span style={style}>shoot</span>!
 			</p>
 			<p>© 2022 copyright</p>
-			<h3>table of content</h3>
-			<h3>Astro party 是高中時熱門遊戲之ㄧ，我們想要透過</h3>
-			<h3>web app 的方式，將遊戲擴展到能夠線上多人遊玩，並利用</h3>
-			<h3>web socket 達成傳輸資料的方法。</h3>
-			<h3>01&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Features</h3>
-			<h3>Features</h3>
 		</div>
 	)
 }
